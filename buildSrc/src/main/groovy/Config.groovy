41149079dd23169d260532d9fb4486f1f0dc287f
timmy <zw_g@foxmail.com>
2019-08-05 23:38:48 +0800

    init project

---
+++ buildSrc/src/main/groovy/Config.groovy
@@ -0,0 +1,15 @@
+class Config {
+
+    static applicationId = 'com.timmy.aucframe'
+    static appName = 'AucFrame'
+
+    static compileSdkVersion = 29
+    static minSdkVersion = 21
+    static targetSdkVersion = 29
+    static versionCode = 1_000_000
+    static versionName = '1.0.0'
+
+    static kotlin_version = '1.3.41'
+    static support_version = '27.1.1'
+    static leakcanary_version = '1.6.3'
+}
